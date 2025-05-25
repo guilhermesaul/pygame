50f7d53adc97d3b04fb8d7d33fc001e38e544c32
guisaul <guilhermesaul190@gmail.com>
2025-05-25 19:03:08 -0300

    controlando o quadrado

--- conteúdo/joao tinti/ex001.py
+++ conteúdo/joao tinti/ex001.py
@@ -7,7 +7,7 @@ pygame.init
 largura = 640
 altura = 480
 x = largura/2
-y = 0
+y = largura/2
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Jogo")
@@ -16,9 +16,7 @@ relogio = pygame.time.Clock() # função para conseguir definir o FPS
 while True:
     # Calcular as regras
     relogio.tick(30) # define o FPS
-    if y - 50 >= altura:
-        y = 0
-    y += 5
+    
     # Pintar
     tela.fill((0, 0, 0))
     pygame.draw.rect(tela, (255, 0, 0), (x, y, 50, 50))
@@ -28,4 +26,21 @@ while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-            exit()  
+            exit()
+        # if event.type == KEYDOWN:
+        #     if event.key == K_a:
+        #         x -= 20
+        #     if event.key == K_d:
+        #         x += 20
+        #     if event.key == K_w:
+        #         y -= 20
+        #     if event.key == K_s:
+        #         y += 20
+    if pygame.key.get_pressed()[K_a]:
+        x -= 20
+    if pygame.key.get_pressed()[K_d]:
+        x += 20
+    if pygame.key.get_pressed()[K_w]:
+        y -= 20
+    if pygame.key.get_pressed()[K_s]:
+        y += 20
